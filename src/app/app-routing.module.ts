c135da42b74342b2ddd6bab806c9b1f8249e2c6a
Adrian Sarac <adrian.sarac@connatix.com>
2022-11-05 13:27:22 +0200

    redirect to auth/login by default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{
-  path: '',
-  children: [
-    // Authentication Management
-    {
-      path: 'auth',
-      loadChildren: () => import('./features/authentication/authentication-routing.module').then(m => m.AuthenticationRoutingModule)
-    },
-  ]
-}];
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full'
+  },
+  // Authentication Management
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/authentication/authentication-routing.module').then(m => m.AuthenticationRoutingModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
